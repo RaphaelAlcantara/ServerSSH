669afa389fbea99befe35f4255779ce3dd02a618
RaphaelAlcantara <rvfra@discente.ifpe.edu.br>
2022-03-29 19:53:00 -0300

    first commit

---
+++ EchoServer.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.time.ZonedDateTime;
+
+public class EchoServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket s = new ServerSocket(8190);
+            Socket incoming = s.accept();
+            try {
+                InputStream inStream = incoming.getInputStream();
+                OutputStream outStream = incoming.getOutputStream();
+
+                PrintWriter out = new PrintWriter(outStream, true /* autoFlush */);
+                out.println("Hello! Enter BYE to exit.");
+                out.println("Hello! Enter 1 to Actual Hour.");
+                out.println("Hello! Enter 2 to Name of System.");
+                Scanner in = new Scanner(inStream);
+
+                // echo client input
+                boolean done = false;
+                //while (!done && in.hasNextLine()) {
+                    while (!done) {
+                    {
+                        String line = in.nextLine();
+                        System.out.println("Cliente " + line);
+                        if (line.trim().equals("BYE"))
+                        done = true;
+                        //out.println("Echo: " + line);
+                    }
+
+                    {
+                        String line = in.nextLine();
+                        if (line.trim().equals("1"))
+                            out.println("A hora atual é: " + ZonedDateTime.now());
+                        done = false;
+                    }
+
+                    {
+                        String line = in.nextLine();
+                        if (line.trim().equals("2"))
+                            out.println(System.getProperty("os.name"));
+                        done = false;
+                    }
+                }
+            } finally {
+                incoming.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
